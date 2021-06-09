c7578078f187cdadfc9c0421cfd20127798857f5
Nhat Nguyen <nhat.nguyen1125@gmail.com>
2021-06-09 22:16:52 +0700

    set up mock source code for each microservices

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,16 +1,23 @@
 "use strict";
 
 const express = require("express");
+const fetch = require("node-fetch");
 
 // Constants
 const PORT = 8080;
-const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/node", async (req, res) => {
+  let result = await fetch("http://node-service-01:8080");
+  result = await result.json();
+  res.send(result);
+});
+app.get("/go", async (req, res) => {
+  let result = await fetch("http://go-service-01:8080");
+  result = await result.json();
+  res.send(result);
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT);
 console.log(`Api gateway service is running on ${PORT} port`);
